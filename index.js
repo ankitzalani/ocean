b279346d267bbb22b19182d115948ad004485e30
Ankit Zalani <azalani@akamai.com>
2016-09-03 19:55:06 +0530

    Changes for crash in 60 seconds.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var login = require("facebook-chat-api");
 
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
 // Simple echo bot. He'll repeat anything that you say.
 // Will stop when you say '/stop'
 
@@ -47,6 +51,51 @@ function intelligence(event, api) {
     }
     return target;
 }
+
+http.createServer(function (request, response) {
+
+   console.log('request starting for ');
+   console.log(request);
+
+   var filePath = '.' + request.url;
+   if (filePath == './')
+       filePath = './index.html';
+
+   console.log(filePath);
+   var extname = path.extname(filePath);
+   var contentType = 'text/html';
+   switch (extname) {
+       case '.js':
+           contentType = 'text/javascript';
+           break;
+       case '.css':
+           contentType = 'text/css';
+           break;
+   }
+
+   path.exists(filePath, function(exists) {
+
+       if (exists) {
+           fs.readFile(filePath, function(error, content) {
+               if (error) {
+                   response.writeHead(500);
+                   response.end();
+               }
+               else {
+                   response.writeHead(200, { 'Content-Type': contentType });
+                   response.end(content, 'utf-8');
+               }
+           });
+       }
+       else {
+           response.writeHead(404);
+           response.end();
+       }
+   });
+
+}).listen(process.env.PORT || 5000);
+
+console.log('Server running at http://127.0.0.1:5000/');
 
 
 // Gets any user based on graph match by facebook.
